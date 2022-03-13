76c7f7e8280268e4cee4bbcc6a43c83434478bb7
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2022-03-13 21:27:20 +0200

    Add files via upload

---
+++ Lab3/Lab3/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lab3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_LAB3_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_LAB3                        107
+#define IDI_SMALL                       108
+#define IDC_LAB3                        109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define ID_TASK_T1                      32771
+#define ID_TASK_T2                      32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
